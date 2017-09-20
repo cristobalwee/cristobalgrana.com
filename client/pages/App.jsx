1c16beaf563f8917da9b23cf05e8a557947966e2
Cristobal Grana <cristobal.wee@gmail.com>
2017-09-20 13:48:18 -0500

    Text animation extension

--- client/pages/App.jsx
+++ client/pages/App.jsx
@@ -14,55 +14,59 @@ import Project from '../components/project.jsx';
 
 class TextScramble {
   constructor(el) {
-    this.el = el
-    this.chars = '!<>-_\\/[]{}—=+*^?#________'
+    this.el = el;
+    this.chars = 'abcdefghijklmnopqrstuvwxyz';
     this.update = this.update.bind(this)
   }
+
   setText(newText) {
-    const oldText = this.el.innerText
-    const length = Math.max(oldText.length, newText.length)
-    const promise = new Promise((resolve) => this.resolve = resolve)
-    this.queue = []
+    const oldText = this.el.innerText;
+    const length = Math.max(oldText.length, newText.length);
+    const promise = new Promise((resolve) => this.resolve = resolve);
+    this.queue = [];
     for (let i = 0; i < length; i++) {
-      const from = oldText[i] || ''
-      const to = newText[i] || ''
-      const start = Math.floor(Math.random() * 40)
-      const end = start + Math.floor(Math.random() * 40)
-      this.queue.push({ from, to, start, end })
+      const from = oldText[i] || '';
+      const to = newText[i] || '';
+      const start = Math.floor(Math.random() * 40);
+      const end = start + Math.floor(Math.random() * 40);
+      this.queue.push({ from, to, start, end });
     }
-    cancelAnimationFrame(this.frameRequest)
-    this.frame = 0
-    this.update()
-    return promise
+
+    cancelAnimationFrame(this.frameRequest);
+    this.frame = 0;
+    this.update();
+    return promise;
   }
+
   update() {
-    let output = ''
-    let complete = 0
+    let output = '';
+    let complete = 0;
     for (let i = 0, n = this.queue.length; i < n; i++) {
-      let { from, to, start, end, char } = this.queue[i]
+      let { from, to, start, end, char } = this.queue[i];
       if (this.frame >= end) {
-        complete++
-        output += to
+        complete++;
+        output += to;
       } else if (this.frame >= start) {
         if (!char || Math.random() < 0.28) {
-          char = this.randomChar()
-          this.queue[i].char = char
+          char = this.randomChar();
+          this.queue[i].char = char;
         }
-        output += `<span class="dud">${char}</span>`
+        output += `<span class="gray">${char}</span>`;
       } else {
-        output += from
+        output += from;
       }
     }
-    this.el.innerHTML = output
+    this.el.innerHTML = output;
+
     if (complete === this.queue.length) {
-      this.resolve()
+      this.resolve();
     } else {
-      this.frameRequest = requestAnimationFrame(this.update)
-      this.frame++
+      this.frameRequest = requestAnimationFrame(this.update);
+      this.frame++;
     }
   }
   randomChar() {
-    return this.chars[Math.floor(Math.random() * this.chars.length)]
+    return this.chars[Math.floor(Math.random() * this.chars.length)];
   }
 }
 
@@ -144,39 +148,24 @@ class App extends Component {
       "Private Pilot",
       "Trivia Enthusiast",
       "Student at UIUC"
-    ]
+    ];
 
-    const el = document.querySelector('.text')
-    const fx = new TextScramble(el)
+    setTimeout(() => {
+      landingSubHead2.addClass("text");
 
-    let counter = 0
-    const next = () => {
-      fx.setText(phrases[counter]).then(() => {
-        setTimeout(next, 800)
-      })
-      counter = (counter + 1) % phrases.length
-    }
+      const el = document.querySelector('.text');
+      const fx = new TextScramble(el);
 
-    next();
+      let counter = 0;
+      const next = () => {
+        fx.setText(phrases[counter]).then(() => {
+          setTimeout(next, 1200);
+        })
+        counter = (counter + 1) % phrases.length;
+      }
 
-    const subtitles = ["Design Technologist",
-                      "UX Engineer",
-                      "Frontend Web Developer",
-                      "UI/UX Designer",
-                      "Private Pilot",
-                      "Trivia Enthusiast",
-                      "Student at UIUC"];
-    // let idx = 0;
-    // setInterval(() => {
-    //   if (idx > 6) {
-    //     idx = 0;
-    //   }
-    //   landingSubHead2.css("opacity", "0");
-    //   setTimeout(() => {
-    //     document.getElementById("landing-2-sub-head").innerHTML = subtitles[idx++];
-    //     landingSubHead2.css("opacity", "1");
-    //   }, 1000);
-    // }, 2500);
+      next();
+    }, 1200);
   }
 
   scroll(down) {
@@ -451,7 +440,7 @@ class App extends Component {
           <div className="content">
             <h1 id="landing-head">Hello there</h1>
             <h3 id="landing-sub-head">My name is Cristobal Graña, and I'm a</h3>
-            <h3 id="landing-2-sub-head" class="text">Student at UIUC</h3>
+            <h3 id="landing-2-sub-head">Student at UIUC</h3>
           </div>
           <div id="mouse-anim" className="bottom center expanded responsivehide">
             <div className="mouse center link" onClick={this.scroll}>
