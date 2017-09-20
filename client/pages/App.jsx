6b462903579eba5024efa6344bbb3e0282e37091
Cristobal Grana <cristobal.wee@gmail.com>
2017-09-20 13:36:10 -0500

    Text animation

--- client/pages/App.jsx
+++ client/pages/App.jsx
@@ -10,6 +10,61 @@ import Project from '../components/project.jsx';
 //https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md#adding-a-css-preprocessor-sass-less-etc
 //http://www.last.fm/api/show/user.getRecentTracks
 //https://support.spotify.com/us/using_spotify/app_integrations/scrobble-to-last-fm/
+//https://codepen.io/soulwire/pen/mErPAK?editors=0010
+
+class TextScramble {
+  constructor(el) {
+    this.el = el
+    this.chars = '!<>-_\\/[]{}—=+*^?#________'
+    this.update = this.update.bind(this)
+  }
+  setText(newText) {
+    const oldText = this.el.innerText
+    const length = Math.max(oldText.length, newText.length)
+    const promise = new Promise((resolve) => this.resolve = resolve)
+    this.queue = []
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || ''
+      const to = newText[i] || ''
+      const start = Math.floor(Math.random() * 40)
+      const end = start + Math.floor(Math.random() * 40)
+      this.queue.push({ from, to, start, end })
+    }
+    cancelAnimationFrame(this.frameRequest)
+    this.frame = 0
+    this.update()
+    return promise
+  }
+  update() {
+    let output = ''
+    let complete = 0
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i]
+      if (this.frame >= end) {
+        complete++
+        output += to
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar()
+          this.queue[i].char = char
+        }
+        output += `<span class="dud">${char}</span>`
+      } else {
+        output += from
+      }
+    }
+    this.el.innerHTML = output
+    if (complete === this.queue.length) {
+      this.resolve()
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update)
+      this.frame++
+    }
+  }
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)]
+  }
+}
 
 class App extends Component {
   constructor(props) {
@@ -80,6 +135,29 @@ class App extends Component {
           break;
         }
     };
+
+    const phrases = [
+      "Design Technologist",
+      "UX Engineer",
+      "Frontend Web Developer",
+      "UI/UX Designer",
+      "Private Pilot",
+      "Trivia Enthusiast",
+      "Student at UIUC"
+    ]
+
+    const el = document.querySelector('.text')
+    const fx = new TextScramble(el)
+
+    let counter = 0
+    const next = () => {
+      fx.setText(phrases[counter]).then(() => {
+        setTimeout(next, 800)
+      })
+      counter = (counter + 1) % phrases.length
+    }
+
+    next();
 
     const subtitles = ["Design Technologist",
                       "UX Engineer",
@@ -373,7 +451,7 @@ class App extends Component {
           <div className="content">
             <h1 id="landing-head">Hello there</h1>
             <h3 id="landing-sub-head">My name is Cristobal Graña, and I'm a</h3>
-            <h3 id="landing-2-sub-head">Student at UIUC</h3>
+            <h3 id="landing-2-sub-head" class="text">Student at UIUC</h3>
           </div>
           <div id="mouse-anim" className="bottom center expanded responsivehide">
             <div className="mouse center link" onClick={this.scroll}>
