3f9349603fd2e176c2ccec15b74f1afe3df479a9
Cristobal Grana <cristobal.wee@gmail.com>
2017-09-20 16:53:52 -0500

    Remove text animation

--- client/pages/App.jsx
+++ client/pages/App.jsx
@@ -11,64 +11,7 @@ import Project from '../components/project.jsx';
 //http://www.last.fm/api/show/user.getRecentTracks
 //https://support.spotify.com/us/using_spotify/app_integrations/scrobble-to-last-fm/
 //https://codepen.io/soulwire/pen/mErPAK?editors=0010
-
-class TextScramble {
-  constructor(el) {
-    this.el = el;
-    this.chars = 'abcdefghijklmnopqrstuvwxyz';
-    this.update = this.update.bind(this)
-  }
-
-  setText(newText) {
-    const oldText = this.el.innerText;
-    const length = Math.max(oldText.length, newText.length);
-    const promise = new Promise((resolve) => this.resolve = resolve);
-    this.queue = [];
-    for (let i = 0; i < length; i++) {
-      const from = oldText[i] || '';
-      const to = newText[i] || '';
-      const start = Math.floor(Math.random() * 80);
-      const end = start + Math.floor(Math.random() * 80);
-      this.queue.push({ from, to, start, end });
-    }
-
-    cancelAnimationFrame(this.frameRequest);
-    this.frame = 0;
-    this.update();
-    return promise;
-  }
-
-  update() {
-    let output = '';
-    let complete = 0;
-    for (let i = 0, n = this.queue.length; i < n; i++) {
-      let { from, to, start, end, char } = this.queue[i];
-      if (this.frame >= end) {
-        complete++;
-        output += to;
-      } else if (this.frame >= start) {
-        if (!char || Math.random() < 0.28) {
-          char = this.randomChar();
-          this.queue[i].char = char;
-        }
-        output += `<span class="gray">${char}</span>`;
-      } else {
-        output += from;
-      }
-    }
-    this.el.innerHTML = output;
-
-    if (complete === this.queue.length) {
-      this.resolve();
-    } else {
-      this.frameRequest = requestAnimationFrame(this.update);
-      this.frame++;
-    }
-  }
-  randomChar() {
-    return this.chars[Math.floor(Math.random() * this.chars.length)];
-  }
-}
+//https://codepen.io/yoannhel/pen/sJpDj?editors=1100
 
 class App extends Component {
   constructor(props) {
@@ -149,23 +92,6 @@ class App extends Component {
       "Trivia Enthusiast",
       "Student at UIUC"
     ];
-
-    // setTimeout(() => {
-    //   landingSubHead2.addClass("text");
-    //
-    //   const el = document.querySelector('.text');
-    //   const fx = new TextScramble(el);
-    //
-    //   let counter = 0;
-    //   const next = () => {
-    //     fx.setText(phrases[counter]).then(() => {
-    //       setTimeout(next, 1200);
-    //     })
-    //     counter = (counter + 1) % phrases.length;
-    //   }
-    //
-    //   next();
-    // }, 1200);
   }
 
   scroll(down) {
